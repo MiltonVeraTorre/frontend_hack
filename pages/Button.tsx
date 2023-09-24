7d8b48bf9c29e0919bef750c30d54b74bde236d9
Aldo Barraza <aldobaar08@gmail.com>
2023-09-24 07:59:55 -0600

    Cambios boton

---
+++ pages/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+        <button
+            type="button"
+            className="bg-gray-700 px-4 py-2 rounded-md text-white-50 font-bold text-xl mt-20"
+            onClick={() => {
+              siguientePaso();
+            }}
+          >
+            Continuar->
+          </button>
